e6710329e9d40fc64af6fdb572f186733d5d5fdb
Jorge Gaitan <jorgedeveloper24@gmail.com>
2020-05-12 01:05:57 -0500

    Creacion de funciones para minar nuevo bloque y convertir un bloque en una cadena de texto

--- blockchain/block.go
+++ blockchain/block.go
@@ -1,27 +1,50 @@
 package blockchain
 
 import (
-	"time" 
-	"sync"
+	"time"
+	"encoding/hex"
+	"crypto/sha256"
+	"encoding/json"
 )
 
 type Block struct {
-	Timestamp 		time.Time
+	Timestamp		string
 	PreviousHash	string
-    Hash			string
+    Hash 			string
     Data 			string
 }
 
-var instance *Block
-var once sync.Once
+func (this *Block) ToString() string {
+	str, _ := json.Marshal(this)
+	return string(str)
+}
 
-func (this *Block) Genesis() *Block{
-   instance = &Block{
-    	Timestamp: time.Now(),
-    	PreviousHash: "empty",
-    	Hash: "g3n3s1s-h4sh",
-    	Data: "g3n3s1s-data",
-    }
+func Genesis() *Block{
+    return GenerateBlock(time.Now(), "3mp7y", "g3n3s1s-h4sh", "g3n3s1s-data")
+}
 
-    return instance
+func GenerateBlock(timestamp time.Time, previousHash string, hash string, data string) *Block {
+
+	newBlock := &Block{Timestamp: timestamp.String(), PreviousHash: previousHash, Hash: hash, Data: data}
+
+    return newBlock
+}
+
+func Mine(previousBlock *Block, data string) *Block{
+	timestamp := time.Now()
+	previousHash := previousBlock.Hash
+
+	hash := calculateHash(timestamp, previousHash, data)
+
+	return GenerateBlock(timestamp, previousHash, hash, data)
+}
+
+func calculateHash(timestamp time.Time, previousHash string, data string) string {
+	record := timestamp.String() + previousHash + string(data)
+
+	h := sha256.New()
+	h.Write([]byte(record))
+	hashed := h.Sum(nil)
+
+	return hex.EncodeToString(hashed)
 }
--- main.go
+++ main.go
@@ -6,11 +6,17 @@ import (
 )
 
 func main(){
-	fmt.Println("Iniciando Blockchain")
+	fmt.Println("\n===================== Iniciando Blockchain =====================\n")
 
-	blockInstance := new(b.Block)
+	//blockInstance := new(b.Block)
 
-	genesisBlock := blockInstance.Genesis();
-	
-	fmt.Println(genesisBlock)
+	genesisBlock := b.Genesis();
+	newBlock1 := b.Mine(genesisBlock, "data 1");
+	newBlock2 := b.Mine(newBlock1, "data 2");
+	newBlock3 := b.Mine(newBlock2, "data 3");
+
+	fmt.Println(genesisBlock.ToString())
+	fmt.Println(newBlock1.ToString())
+	fmt.Println(newBlock2.ToString())
+	fmt.Println(newBlock3.ToString())
 }
